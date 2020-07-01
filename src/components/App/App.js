3964eb8b0cc438f55e728a3191cd1307895c98e0
wari.1986 <nicolaycamacho@gmail.com>
2020-07-01 18:57:44 -0400

    Created App, SearchBar, SearchResults components and a render method for each, also added the HTML for each return.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,10 +10,10 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          // <SearchBar />
+          {/* <SearchBar /> */}
           <div className="App-playlist">
-              // <SearchResults />
-              // <Playlist />
+              {/* <SearchResults /> */}
+              {/* <Playlist /> */}
           </div>
         </div>
       </div>
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SearchBar';
+
+
+
+class SearchBar extends React.Component {
+  render(){
+    return(
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <button className="SearchButton">SEARCH</button>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
